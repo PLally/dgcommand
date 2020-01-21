622b1a009f117d3aefb448c3f26e4a4a1c961346
PLlally <11771225+PLally@users.noreply.github.com>
2020-01-21 17:48:54 -0500

    add embed helpers

--- embed/embd_helpers.go
+++ embed/embd_helpers.go
@@ -1,1 +1,36 @@
 package embed
+
+import "github.com/bwmarrin/discordgo"
+
+type Embed struct {
+	*discordgo.MessageEmbed
+}
+
+func NewEmbed() *Embed {
+	return &Embed{&discordgo.MessageEmbed{}}
+}
+
+func (e *Embed) AddField(name, value string, inline bool) {
+	e.Fields = append(e.Fields, &discordgo.MessageEmbedField{
+		Name:   name,
+		Value:  value,
+		Inline: inline,
+	})
+}
+
+func (e *Embed) SetTitle(title, url string) {
+	e.Title = title
+	e.URL = url
+}
+
+func (e *Embed) SetImageUrl(url string) {
+	e.Image = &discordgo.MessageEmbedImage{
+		URL: url,
+	}
+}
+
+func (e *Embed) SetThumbnailUrl(url string) {
+	e.Thumbnail = &discordgo.MessageEmbedThumbnail{
+		URL: url,
+	}
+}
